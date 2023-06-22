63e125beaf3bb6a2e580f9d2403e423ba69f75ee
Rens van Gelder <rvgelder@sogyo.nl>
2023-06-22 14:32:54 +0200

    Initial commit with spring-boot, maven and testing integrated

---
+++ src/test/java/com/sogyo/rvgelder/ipdebitcardreplacementflow/MainTests.java
@@ -0,0 +1,19 @@
+package com.sogyo.rvgelder.ipdebitcardreplacementflow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.fail;
+
+@SpringBootTest
+class MainTests {
+
+	@Test
+	void TestingWorks() {
+
+		assertTrue(true);
+	}
+
+
+}

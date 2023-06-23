2d41c2b0b8707f3fc5ac1d9eaa68c398ff867b26
Rens van Gelder <rvgelder@sogyo.nl>
2023-06-23 14:36:11 +0200

    Added Cards, Customer and CardArrangement tables to database, with a relationship to each other (Cards and Customer to CardArrangement).

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -9,24 +9,39 @@
 --TODO - Table Card_Arrangement - Link card_id to corresponding Card
 --TODO - Table Customer - allowed_actions - get an enum array working, with the allowed actions
 CREATE TABLE IF NOT EXISTS Card(
---card_id INTEGER auto_increment,
-card_id SERIAL PRIMARY KEY,
-card_arrangement_id INTEGER auto_increment,
+id SERIAL PRIMARY KEY,
+card_arrangement_id INTEGER UNIQUE NOT NULL,
 start_date DATE NOT NULL,
 end_date DATE NOT NULL,
 status ENUM('active', 'inactive', 'blocked', 'expired') NOT NULL
 );
 
 CREATE TABLE IF NOT EXISTS Customer(
---customer_id INTEGER auto_increment,
-customer_id SERIAL PRIMARY KEY,
+id SERIAL PRIMARY KEY,
+card_arrangement_id INTEGER UNIQUE NOT NULL,
 allowed_actions ENUM('replace', 'changeLimit', 'issue', 'block', 'unblock') ARRAY,
 authorization_level ENUM('level1','level2','level3','level4','level5')
 );
 
 CREATE TABLE IF NOT EXISTS CardArrangement(
---card_arrangement_id INTEGER auto_increment,
-card_arrangement_id SERIAL PRIMARY KEY,
-card_id INTEGER NOT NULL,
-customer_id INTEGER NOT NULL
+id SERIAL PRIMARY KEY,
+card_id INTEGER UNIQUE NOT NULL,
+customer_id INTEGER UNIQUE NOT NULL
 );
+
+ALTER TABLE Card
+ADD CONSTRAINT FK_Card_CardArrangement FOREIGN KEY (card_arrangement_id)
+REFERENCES CardArrangement(id);
+
+ALTER TABLE CardArrangement
+ADD CONSTRAINT FK_CardArrangement_Card FOREIGN KEY (card_id)
+REFERENCES Card(id);
+
+ALTER TABLE CardArrangement
+ADD CONSTRAINT FK_CardArrangement_Customer FOREIGN KEY (customer_id)
+REFERENCES Customer(id);
+
+ALTER TABLE Customer
+ADD CONSTRAINT FK_Customer_CardArrangement FOREIGN KEY (card_arrangement_id)
+REFERENCES CardArrangement(id);
+

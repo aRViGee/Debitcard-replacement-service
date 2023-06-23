d428a39f4889756e7d4ecd69cf33104c6d5755be
Rens van Gelder <rvgelder@sogyo.nl>
2023-06-23 16:58:37 +0200

    Added Card.java

---
+++ src/test/java/com/sogyo/rvgelder/ipdebitcardreplacementflow/Card.java
@@ -0,0 +1,34 @@
+package com.sogyo.rvgelder.ipdebitcardreplacementflow;
+
+public class Card {
+    private final int cardID;
+    private final int cardArrangementID;
+    private final String startDate; /*TODO How do I convert this to the database's 'date' format? Separate the month number and year?*/
+    private final String endDate; /*TODO idem TODO startDate*/
+    private Enum status;
+
+    public Card(
+            int cardID,
+            int cardArrangementID,
+            String startDate,
+            String endDate,
+            Enum status
+    ) {
+        this.cardID = cardID;
+        this.cardArrangementID = cardArrangementID;
+        this.startDate = startDate;
+        this.endDate = endDate;
+        this.status = status;
+    }
+
+    @Override
+    public String toString() {
+        return "Card{" +
+                "cardID=" + cardID +
+                ", cardArrangementID=" + cardArrangementID +
+                ", startDate='" + startDate + '\'' +
+                ", endDate='" + endDate + '\'' +
+                ", status=" + status +
+                '}';
+    }
+}

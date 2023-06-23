aea2a1374397ffed08ad2392f86fddeab7340f9c
Rens van Gelder <rvgelder@sogyo.nl>
2023-06-23 12:39:45 +0200

    added H2 database and it's dependencies, created Card and Customer tables

--- src/main/java/com/sogyo/rvgelder/ipdebitcardreplacementflow/Main.java
+++ src/main/java/com/sogyo/rvgelder/ipdebitcardreplacementflow/Main.java
@@ -2,12 +2,14 @@ package com.sogyo.rvgelder.ipdebitcardreplacementflow;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication
 public class Main {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Main.class, args);
+
 	}
 
 }
---
+++ src/main/resources/schema.sql
@@ -0,0 +1,32 @@
+--create table book(
+--id int auto_increment,
+--title varchar(255) NOT NULL,
+--pages int NOT NULL,
+--author varchar(255) NOT NULL
+--);
+
+--TODO - Table Card - Link card_arrangement_id to corresponding card_arrangement
+--TODO - Table Card_Arrangement - Link card_id to corresponding Card
+--TODO - Table Customer - allowed_actions - get an enum array working, with the allowed actions
+CREATE TABLE IF NOT EXISTS Card(
+--card_id INTEGER auto_increment,
+card_id SERIAL PRIMARY KEY,
+card_arrangement_id INTEGER auto_increment,
+start_date DATE NOT NULL,
+end_date DATE NOT NULL,
+status ENUM('active', 'inactive', 'blocked', 'expired') NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS Customer(
+--customer_id INTEGER auto_increment,
+customer_id SERIAL PRIMARY KEY,
+allowed_actions ENUM('replace', 'changeLimit', 'issue', 'block', 'unblock') ARRAY,
+authorization_level ENUM('level1','level2','level3','level4','level5')
+)
+
+CREATE TABLE IF NOT EXISTS CardArrangement(
+--card_arrangement_id INTEGER auto_increment,
+--card_arrangement_id SERIAL PRIMARY KEY,
+--card_id INTEGER KEY(Card),
+customer_id INTEGER NOT NULL,
+)

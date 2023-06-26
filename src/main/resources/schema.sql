1a51eb2c54d599362a48595931aa99f6bf4f2b60
Rens van Gelder <rvgelder@sogyo.nl>
2023-06-26 13:39:34 +0200

    Added multiple Customers, Cards and CardArrangements in schema.sql. Also small update on MainTests.java.

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -6,40 +6,59 @@
 --);
 
 --TODO Add example Card(s), Customer(s) and CardArrangement(s) for testing
-CREATE TABLE IF NOT EXISTS Card(
-id SERIAL PRIMARY KEY,
-card_arrangement_id INTEGER UNIQUE NOT NULL,
-start_date DATE NOT NULL,
-end_date DATE NOT NULL,
-status ENUM('active', 'inactive', 'blocked', 'expired') NOT NULL
-);
 
 CREATE TABLE IF NOT EXISTS Customer(
-id SERIAL PRIMARY KEY,
-card_arrangement_id INTEGER UNIQUE NOT NULL,
-allowed_actions ENUM('replace', 'changeLimit', 'issue', 'block', 'unblock') ARRAY,
-authorization_level ENUM('level1','level2','level3','level4','level5')
+id SERIAL NOT NULL PRIMARY KEY,
+allowed_actions ENUM('REPLACE', 'CHANGE_LIMIT', 'ISSUE', 'BLOCK', 'UNBLOCK') ARRAY,
+authorization_level ENUM('1','2','3')
 );
 
 CREATE TABLE IF NOT EXISTS CardArrangement(
-id SERIAL PRIMARY KEY,
-card_id INTEGER UNIQUE NOT NULL,
-customer_id INTEGER UNIQUE NOT NULL
+id SERIAL NOT NULL PRIMARY KEY,
+customer_id INTEGER NOT NULL,
+CONSTRAINT FK_CardArrangement_Customer FOREIGN KEY (customer_id) REFERENCES Customer(id)
 );
 
-ALTER TABLE Card
-ADD CONSTRAINT FK_Card_CardArrangement FOREIGN KEY (card_arrangement_id)
-REFERENCES CardArrangement(id);
 
-ALTER TABLE CardArrangement
-ADD CONSTRAINT FK_CardArrangement_Card FOREIGN KEY (card_id)
-REFERENCES Card(id);
+CREATE TABLE IF NOT EXISTS Card(
+id SERIAL NOT NULL PRIMARY KEY,
+card_arrangement_id INTEGER UNIQUE NOT NULL,
+start_date DATE NOT NULL,
+end_date DATE NOT NULL,
+status ENUM('ACTIVE', 'INACTIVE', 'BLOCKED', 'EXPIRED') NOT NULL,
+CONSTRAINT FK_Card_CardArrangement FOREIGN KEY (card_arrangement_id) REFERENCES CardArrangement(id)
+);
 
-ALTER TABLE CardArrangement
-ADD CONSTRAINT FK_CardArrangement_Customer FOREIGN KEY (customer_id)
-REFERENCES Customer(id);
+--ALTER TABLE Card
+--ADD CONSTRAINT FK_Card_CardArrangement FOREIGN KEY (card_arrangement_id)
+--REFERENCES CardArrangement(id);
 
-ALTER TABLE Customer
-ADD CONSTRAINT FK_Customer_CardArrangement FOREIGN KEY (card_arrangement_id)
-REFERENCES CardArrangement(id);
 
+
+--Fill the tables
+INSERT INTO Customer(allowed_actions,authorization_level)
+VALUES(ARRAY['BLOCK','UNBLOCK'],'1');
+
+INSERT INTO CardArrangement(customer_id)
+VALUES(1);
+
+INSERT INTO Card(card_arrangement_id, start_date, end_date, status)
+VALUES(1, '2023-04-01', '2028-04-01', 'ACTIVE');
+
+INSERT INTO Customer(allowed_actions,authorization_level)
+VALUES(ARRAY['REPLACE', 'CHANGE_LIMIT', 'BLOCK', 'UNBLOCK'],'2');
+
+INSERT INTO CardArrangement(customer_id)
+VALUES(2);
+
+INSERT INTO Card(card_arrangement_id, start_date, end_date, status)
+VALUES(2, '2023-05-01', '2028-05-01', 'BLOCKED');
+
+INSERT INTO Customer(allowed_actions,authorization_level)
+VALUES(ARRAY['REPLACE', 'CHANGE_LIMIT', 'ISSUE', 'BLOCK', 'UNBLOCK'],'3');
+
+INSERT INTO CardArrangement(customer_id)
+VALUES(3);
+
+INSERT INTO Card(card_arrangement_id, start_date, end_date, status)
+VALUES(3, '2023-08-01', '2028-08-01', 'INACTIVE');
--- src/test/java/com/sogyo/rvgelder/ipdebitcardreplacementflow/MainTests.java
+++ src/test/java/com/sogyo/rvgelder/ipdebitcardreplacementflow/MainTests.java
@@ -3,8 +3,7 @@ package com.sogyo.rvgelder.ipdebitcardreplacementflow;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-import static org.junit.jupiter.api.Assertions.assertTrue;
-import static org.junit.jupiter.api.Assertions.fail;
+import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
 class MainTests {
